06c76f8b3dc8083d4ea95fb88e034d834c64ca8b
suna <s20183193@seps9.deu.ac.kr>
2020-07-24 17:25:00 +0900

    create 1436_영화감독 숌

--- 탐색/1436_영화감독 숌.cpp
+++ 탐색/1436_영화감독 숌.cpp
@@ -1,11 +1,31 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main() {
-	int N;
+	int N, tmp = 665, count = 0, origintmp;
+	int state = 0;
 
-	cin >> N;
-
-	if (N == 1) cout << "666" << endl;
-	else cout << N-1 << "666" << endl;
+	
+		cin >> N;
+		origintmp = tmp;
+		while (1) {
+			if (tmp > 0) {
+				if (tmp % 10 == 6) {
+					state++;
+					if (state == 3) {
+						count++;
+						if (count == N) break;
+					}
+				}
+				else state = 0;
+				tmp = tmp / 10;
+			}
+			else {
+				tmp = origintmp++;
+				state = 0;
+			}
+		}
+		cout << origintmp - 1 << endl;
+	
 }
